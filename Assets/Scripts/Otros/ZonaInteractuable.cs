0d3cdf3b97c38bbbf98d99dc3e1d0281b085357d
SaulPachecoTrilles <78241212+Salix8@users.noreply.github.com>
2024-10-31 00:23:38 +0100

    Primer intento de seguir la ruta ademas el trigger input va mal se dispara simpre

--- Assets/Scripts/Otros/ZonaInteractuable.cs
+++ Assets/Scripts/Otros/ZonaInteractuable.cs
@@ -1,18 +1,25 @@
+using System.Globalization;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+
 
 public class ZonaInteractuable : MonoBehaviour
 {
     [SerializeField] private ControlCursor controlCursor;
+    [SerializeField] private TextMeshProUGUI textInfo;
+    [SerializeField] private string textoInteraccion;
 
     private void OnMouseEnter()
     {
         controlCursor.changeCursor("mano");
+        textInfo.text = textoInteraccion;
     }
 
     private void OnMouseExit()
     {
         controlCursor.changeCursor("normal");
+        textInfo.text = "";
     }
 }
