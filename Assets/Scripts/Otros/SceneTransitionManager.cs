d128182cf5711360ec2ec680008d5def95a0dfd8
Minecoft34 <67107763+Minecoft34@users.noreply.github.com>
2024-11-27 09:38:14 +0100

    cambiado texto de cambio de escena y completado scene mapping

--- Assets/Scripts/Otros/SceneTransitionManager.cs
+++ Assets/Scripts/Otros/SceneTransitionManager.cs
@@ -5,10 +5,34 @@ public class SceneTransitionManager : MonoBehaviour
 {
     private Dictionary<string, string> sceneMapping = new Dictionary<string, string>
     {
-        { "MurallaPresente", "MurallaPasado" },
-        { "MurallaPasado", "MurallaPresente" },
-        { "BorrullPresente", "BorrullPasado" },
-        { "BorrullPasado", "BorrullPresente" }
+        { "Villores1", "Villores2"                  },
+        { "Villores2", "Villores3Presente"          },
+        { "Villores3Presente", "Villores3Pasado"    },
+        { "Villores3Pasado", "Villores1"            },
+        { "Villores1", "PlazaPresente"              },
+        { "PlazaPresente", "PlazaPasado"            },
+        { "PlazaPasado", "PlazaPresente"            },
+        { "PlazaPresente", "ArciprestalPresente"    },
+        { "ArciprestalPresente", "ArciprestalPasado"},
+        { "ArciprestalPasado", "ArciprestalPresente"},
+        { "MurallaPresente", "MurallaPasado"        },
+        { "MurallaPasado", "MurallaPresente"        },
+        { "BorrullPresente", "BorrullPasado"        },
+        { "BorrullPasado", "BorrullPresente"        },
+        { "CallejonPresente", "CallejonPasado"      },
+        { "CallejonPasado", "CallejonPresente"      },
+        { "HornoPresente", "HornoPasado"            },
+        { "HornoPasado", "HornoPresente"            },
+        { "FuentePasado", "FuentePresente"          },
+        { "FuentePresente", "FuentePasado"          },
+        { "PerePresente", "PerePasado"              },
+        { "PerePasado", "PerePresente"              },
+        { "ConventoPresente", "ConventoPasado"      },
+        { "ConventoPasado", "ConventoPresente"      },
+        { "ConventoPresente", "ConventoGalletas"    },
+        { "ConventoGalletas", "Villores1"           }
+
+
     };
 
     public string GetOtherScene(string currentScene)
