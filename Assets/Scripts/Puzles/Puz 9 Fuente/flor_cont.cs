8dd4ee1f6adcf83e70d228c0731fcf25e163093f
Leo-Cabrera <kanomody29@gmail.com>
2024-11-27 22:38:51 +0100

    Puzzle flores
    Primera versión puzzles flores

---
+++ Assets/Scripts/Puzles/Puz 9 Fuente/flor_cont.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class flor_cont : MonoBehaviour
+{
+    Rigidbody2D rigidbody2d;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+        rigidbody2d = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 position = GetComponent<Rigidbody2D>().position;
+
+    
+        if(position.y < -8)
+        {
+            Destroy(gameObject);
+        }
+    }
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        cesto_move cesto  = other.collider.GetComponent<cesto_move >();
+
+        cesto.points+=1;
+        Debug.Log("Puntos: " + cesto.points);
+
+        Destroy(gameObject);   
+    }
+}
