da596e3642bb065aa11479e57b8723c36510337f
JaumeLloretRubio <al425988@uji.es>
2024-11-28 01:52:54 +0100

    Flujo principal completado, sprites de los npcs y placeholders de los botones para los minijuegos

--- Assets/Scripts/Otros/SceneTransitionManager.cs
+++ Assets/Scripts/Otros/SceneTransitionManager.cs
@@ -8,7 +8,7 @@ public class SceneTransitionManager : MonoBehaviour
         { "Villores1", "Villores2"                  },
         { "Villores2", "Villores3Presente"          },
         { "Villores3Presente", "Villores3Pasado"    },
-        { "Villores3Pasado", "Villores1"            },
+        { "Villores3Pasado", "PlazaPresente"        },
         { "Villores1", "PlazaPresente"              },
         { "PlazaPresente", "PlazaPasado"            },
         { "PlazaPasado", "PlazaPresente"            },
