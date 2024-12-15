e0eeafe86796aedf51da949ab219b53805e3f64b
Minecoft34 <67107763+Minecoft34@users.noreply.github.com>
2024-12-15 16:02:46 +0100

    Añadido efectos de sonido a botones y flechas

--- Assets/Scripts/Otros/ChangeZone.cs
+++ Assets/Scripts/Otros/ChangeZone.cs
@@ -6,6 +6,15 @@ using UnityEngine;
 public class ChangeZone : MonoBehaviour
 {
     [SerializeField] private GameObject nextZone;
+    [SerializeField] private AudioClip interactionSound; // Sonido al interactuar
+    [SerializeField] private bool playSound = true; // Controla si se debe reproducir el sonido
+    [SerializeField] private AudioSource audioSource;
+
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+        audioSource.clip = interactionSound;
+    }
 
     public void OnMouseUp()
     {
@@ -15,7 +24,22 @@ public class ChangeZone : MonoBehaviour
             transform.parent.gameObject.SetActive(false);
             nextZone.gameObject.SetActive(true);
             GlobalManager.GetInstance().SetPathObject(false);
+            GameObject audio = new GameObject("AudioFlecha");
+            AudioSource audioSource = audio.AddComponent<AudioSource>();
+            audioSource.clip = interactionSound;
+            audioSource.Play();
+
+            StartCoroutine(DestroyAfterSound(audioSource));
         }
+    }
+
+    private IEnumerator DestroyAfterSound(AudioSource audioSource)
+    {
+        // Esperar a que el audio termine
+        yield return new WaitForSeconds(audioSource.clip.length);
+
+        // Destruir el GameObject
+        Destroy(audioSource.gameObject);
     }
 }
 
