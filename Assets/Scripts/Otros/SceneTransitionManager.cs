ad20f5b4ec0221bbf828d4f20833a493ac6e70a2
Minecoft34 <67107763+Minecoft34@users.noreply.github.com>
2024-12-22 23:32:07 +0100

    Muchos cambios pt2
    añadido dialogos post puzzle al array de progression manager, cambiado titulos para que su borde sea oscuro, arreglado boton de salir, arreglado feedback visual del fucking cursor, unificado scripts de cambiomenu y scenetransitionmanager, añadido q el movil se quite al cambiar de tiempo, bajado texto de informacion de movimiento, añadido que se quite el movil al abrir el mapa y añadido sonido a botón de volver del mapa, borrado script de cambiomenu (tengo una copia por si acaso)

--- Assets/Scripts/Otros/SceneTransitionManager.cs
+++ Assets/Scripts/Otros/SceneTransitionManager.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class SceneTransitionManager : MonoBehaviour
 {
+    [SerializeField] private GameObject[] allGameObjects;    
+    [SerializeField] private GameObject menuTelefono;       
+    [SerializeField] private GameObject mapa;  
     private Dictionary<string, string> zoneMapping = new Dictionary<string, string>
     {
         { "Villores3Presente", "Villores3Pasado"    },
@@ -27,6 +30,51 @@ public class SceneTransitionManager : MonoBehaviour
         { "ConventoPresente", "ConventoPasado"      },
         { "ConventoPasado", "ConventoPresente"      }
     };
+
+    void Start()
+    {
+        menuTelefono.SetActive(false);
+        mapa.SetActive(false);
+    }
+
+    public void AbrirMenuTelefono() // no se abre cuando hay dialogo, desactiva cambio de Zona
+    {
+        Debug.Log($"Hay dialogo? {DialogueManager.GetInstance().dialogueIsPlaying}");
+        if (!DialogueManager.GetInstance().dialogueIsPlaying)
+        {
+            Debug.Log($"1 No hay dialogo");
+            foreach (GameObject obj in allGameObjects)
+            {
+                Debug.Log($"{obj}");
+                obj.SetActive(true);
+
+            }
+            Debug.Log($"2");
+
+            GlobalManager.GetInstance().SetPathObject(false);
+            menuTelefono.SetActive(true);
+            Debug.Log($"3 {menuTelefono}");
+        }
+    }
+
+    public void MostrarMapa()
+    {
+        mapa.SetActive(true);
+        menuTelefono.SetActive(false);
+    }
+
+    
+    public void Volver()
+    {
+        menuTelefono.SetActive(false);
+        mapa.SetActive(false);
+
+        
+        foreach (GameObject obj in allGameObjects)
+        {
+            obj.SetActive(true);
+        }
+    }
 
     public string GetOtherZone(string currentZone)
     {
@@ -70,6 +118,7 @@ public class SceneTransitionManager : MonoBehaviour
                 // Desactivamos la escena actual y activamos la otra escena
                 currentZone.SetActive(false);
                 otherZone.SetActive(true);
+                menuTelefono.SetActive(false);
             }
             else
             {
