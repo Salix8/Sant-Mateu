bb7d54fc7106dee2ff02ea865d49dfb0e8173ea1
Minecoft34 <67107763+Minecoft34@users.noreply.github.com>
2025-01-21 17:02:48 +0100

    puzzle fuente
    añadido puntos visuales

--- Assets/Scripts/Puzles/Puz 9 Fuente/cesto_move.cs
+++ Assets/Scripts/Puzles/Puz 9 Fuente/cesto_move.cs
@@ -4,29 +4,18 @@ using UnityEngine;
 
 public class cesto_move : MonoBehaviour
 {
-
-    public int points = 0;
+    public int points = 0; // Puntos totales
     Rigidbody2D rb2D;
 
-    // Start is called before the first frame update
     void Start()
     {
         rb2D = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void Movement(float value)
     {
-        
-    }
-
-    public void Movement (float value){
         Vector2 position = rb2D.position;
-
-        position.x = value/10;
-
+        position.x = value / 10;
         rb2D.MovePosition(position);
-
-        
     }
 }
